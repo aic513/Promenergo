f541eda0cef4e6c14aaefb294398eb9bd9274782
aic513 <aic513@mail.ru>
2017-04-06 16:23:26 +0300

    Нарезал файлы для пользовательской части

---
+++ template/default/catalog_page.php
@@ -0,0 +1,87 @@
+<td class="content">
+
+    <h1>
+        Каталог продукции
+    </h1>
+    <div class="kat_map">
+        <a href="#">Арматура для газоснабжения</a> / <a href="#">Клапаны предохранительные сбросные</a>
+    </div>
+
+    <div class="product-cat-main">
+        <div class="product-cat">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+    <div class="product-cat-main">
+        <div class="product-cat">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+    <div class="product-cat-main">
+        <div class="product-cat-third">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+
+
+    <div class="product-cat-main">
+        <div class="product-cat">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+    <div class="product-cat-main">
+        <div class="product-cat">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+    <div class="product-cat-main">
+        <div class="product-cat-third">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+
+
+    <div class="product-cat-main">
+        <div class="product-cat">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+    <div class="product-cat-main">
+        <div class="product-cat">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+    <div class="product-cat-main">
+        <div class="product-cat-third">
+            <a href="#"><img src="images/rpodukt.jpg" alt="Клапан предохранительный сбросной КПС-50" /></a>
+            <a href="#">Клапан предохранительный сбросной КПС-50</a>
+        </div>
+        <div class="bord-bot"></div>
+    </div>
+
+    <div class="clr"></div>
+
+    <ul class="pager">
+        <li><span>1</span></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+    </ul>
+
+</td>
