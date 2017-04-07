721fb38446a6c80faf70c56e01c3cd0bdeee9ede
aic513 <aic513@mail.ru>
2017-04-07 09:41:59 +0300

    Создал базовые константы,настроил .htaccess,настроил index.php

--- config.php
+++ config.php
@@ -2,3 +2,55 @@
 /*
  * Файл с настройками сайта
  */
+
+/*
+ *Константы путей к папкам
+ */
+defined('PROM') or exit('Access denied');  //проверяем на существование константу PROM,чтобы никто не имел доступа к файлам
+
+define('CONTROLLER', 'core/controller');  //путь к контроллеру
+
+define('MODEL', 'core/model');  //путь к модели
+
+define('VIEW', 'template/default');  //путь к шаблону
+
+define('LIB','lib');  //путь к библиотекам
+
+define('SITE_URL', '/promenergo.loc');  //базовый путь к сайту
+
+define('QUANTITY', 5);  //количество выводимых товаров при постраничной навигации
+
+define('QUANTITY_LINKS', 3);  //ссылки при постраничной навигации
+
+define('UPLOAD_DIR', 'images/');  //путь к папке с картинками товаров
+
+/*
+ * Константы подключения к БД
+ */
+define('HOST', 'localhost');
+
+define('USER', 'Denis');
+
+define('PASSWORD', 1234);
+
+define('DB_NAME', 'promenergo');
+
+$conf = array(   //массив с настройками для подключения скриптов и стилей в header
+    'styles' => array(  //пользовательская часть
+        'style.css'
+    ),
+    'scripts' => array(
+        'JS/jquery-1.7.2.min.js',
+        'JS/jquery-ui-1.8.20.custom.min.js',
+        'JS/jquery.cookie.js',
+        'JS/js.js',
+        'JS/script.js',
+    ),
+    'styles_admin' => array(  //админская часть
+        'style.css'
+    ),
+    'scripts_admin' => array(
+        'JS/tiny_mce/tiny_mce.js',
+        'JS/tiny_script.js',
+    ),
+); 
--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
+define('PROM',TRUE);  //закрываем доступ к папкам с помощью этой константы
+
+header('Content-Type:text/html;charset=utf-8');
+
+session_start();
+
+require 'config.php';
+/*
+ * подключаем файлы и библиотеки
+ */
+set_include_path(get_include_path()  //задаем пути для покдлючения файлов,php сам будет перебирать пути,пока не найдет нужный
+    .PATH_SEPARATOR.CONTROLLER
+    .PATH_SEPARATOR.MODEL
+    .PATH_SEPARATOR.LIB
+);
+
+function __autoload($class_name){
+    if(!include_once($class_name.'php')){
+        try{
+            throw new ContrExeption ('Неправильный файл для подключения');
+        }catch (CouchbaseException $e){
+            echo $e->getMessage();
+        }
+    }
+}
+
 
