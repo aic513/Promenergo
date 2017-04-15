ea40530ee27f284b74879f6d72fe712644cdcab5
aic513 <aic513@mail.ru>
2017-04-15 20:52:57 +0300

    Сделал вывод карты сайта через иконку слева

---
+++ core/controller/Map_Controller.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Класс, который выводит страницу карты сайта
+ */
+
+class Map_Controller extends Base
+{
+	protected function input()
+	{
+		parent::input();
+		$this->title .= "Карта сайта";
+
+		$this->pages = $this->ob_m->get_pages();  //получаем массив страниц
+
+		$this->catalog = $this->ob_m->get_catalog_brands();  //получаем массив категорий
+
+		$this->keywords = "Карта сайта";
+		$this->discription = "Промстрой энерго карта сайта";
+
+	}
+
+	protected function output()
+	{
+		$this->content = $this->render(VIEW . 'sitemap_page',
+			array(
+				'pages' => $this->pages,
+				'catalog' => $this->catalog
+			)
+		);
+
+		$this->page = parent::output();
+
+		return $this->page;
+	}
+}
--- template/default/sitemap_page.php
+++ template/default/sitemap_page.php
@@ -1,32 +1,53 @@
 <td class="content">
-
-    <h1>
-        Карта сайта
-    </h1>
-
-    <ul>
-        <li><a href="#">Главная</a></li>
-        <li><a href="#">Новости</a></li>
-        <li><a href="#">О компании</a></li>
-        <li><a href="#">Контакты</a></li>
-        <li><a href="#">Каталог товаров</a>
-            <ul>
-                <li><a href="#">Трубы России</a>
-                    <ul>
-                        <li><a href="#">Счетчики</a></li>
-                        <li><a href="#">Трубы</a></li>
-                        <li><a href="#">Сварные изделия</a>
-                    </ul>
-                </li>
-                <li><a href="#">ГазПром</a>
-                    <ul>
-                        <li><a href="#">Счетчики</a></li>
-                        <li><a href="#">Трубы</a></li>
-                        <li><a href="#">Сварные изделия</a>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-    </ul>
+	
+	<h1>
+		Карта сайта
+	</h1>
+	
+	<? if ($pages && $catalog) : ?>
+		<ul>
+			<li>
+				<a href="<?= SITE_URL; ?>">Главная</a>
+			</li>
+			<li>
+				<a href="<?= SITE_URL; ?>archive">Новости</a>
+			</li>
+			
+			<? foreach ($pages as $item) : ?>
+				<li>
+					<a href="<?= SITE_URL; ?>page/id/<?= $item[ 'page_id' ] ?>"><?= $item[ 'title' ] ?></a>
+				</li>
+			<? endforeach; ?>
+			<li>
+				<a href="<?= SITE_URL; ?>catalog">Каталог товаров</a>
+			</li>
+			<ul>
+				
+				<? foreach ($catalog as $key => $val) : ?>
+					<? if ($val[ 'next_lvl' ]) : ?>
+						<li>
+							<a href="<?= SITE_URL; ?>catalog/parent/<?= $key; ?>"><?= $val[ 0 ] ?></a>
+							<ul>
+								<? foreach ($val[ 'next_lvl' ] as $k => $v) : ?>
+									<li>
+										<a href="<?= SITE_URL; ?>catalog/brand/<?= $k ?>"><?= $v; ?></a>
+									</li>
+								<? endforeach; ?>
+							</ul>
+						</li>
+					<? else : ?>
+						<li>
+							<a href="<?= SITE_URL; ?>catalog/brand/<?= $key ?>"><?= $val[ 0 ]; ?></a>
+						</li>
+					<? endif; ?>
+				<? endforeach; ?>
+			</ul>
+		
+		</ul>
+	
+	
+	<? else : ?>
+		<p>Данных по карте сайта нет</p>
+	<? endif; ?>
 
 </td>
